043ab5123795c0a81e93d98479b528b15288123d
kevin-vargas <kevinvargasnavia@gmail.com>
2023-01-04 00:46:48 -0300

    feat: add web server

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import machine
 import network
 from network import STA_IF
 import secret
-
+from microdot import Microdot
 import socket
 import time
 
 led = machine.Pin("LED", machine.Pin.OUT)
 led.off()
 
-
 server = ('192.168.0.59', 38899)
-#server = ('192.168.0.210', 2115)
+# server = ('192.168.0.210', 2115)
 turn_on = b'{"method":"setPilot","params":{"r":238,"b":2,"g":210,"dimming":10}}'
 turn_off = b'{"method":"setPilot","params":{"state":false}}'
 
@@ -23,7 +22,6 @@ def connect_wifi():
     while not wlan.isconnected():
         print('Waiting for connection...')
         time.sleep(1)
-    led.on()
     return wlan.ifconfig()[0]
 
 
@@ -31,14 +29,22 @@ class Conn:
     def __init__(self, server):
         self.server = server
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
     def send(self, data):
         self.sock.sendto(data, self.server)
 
 
-c = Conn(server)
 connect_wifi()
-c.send(turn_on)
-time.sleep(2)
-c.send(turn_off)
+led.on()
+
+app = Microdot()
+c = Conn(server)
+
+@app.route('/')
+def index(request):
+    print(request.body)
+    c.send(request.body)
+    return 'Hello, world!'
 
 
+app.run(port=80)
